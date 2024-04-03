2a9df8ceb01a499da11fa9dea271093797ece516
Arman <amd452230@gmail.com>
2024-04-03 19:05:22 +0600

    tailwind-css-project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      boxShadow:{
+        '3xl': '0 4px 8px 5px rgba(200, 190, 200, 09)',
+      }
+    },
+  },
+  plugins: [],
+}
+
